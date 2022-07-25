b06d290e280ae75541b6980d44df3eff1e0609d8
Nutty4616 <nkosin361@gmail.com>
2022-07-25 14:43:06 +0200

    Introduced RXJS and Applying it to wikipediaService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { WikipediaService } from './wikipedia.service';
 
+interface Search {
+  title: string;
+  snippet: string;
+  pageid: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pages = [];
+  pages: Search[] = [];
 
   constructor(private wikipedia: WikipediaService) { }
 
   onTerm(term: string) {
-    this.wikipedia.search(term).subscribe((response: any) => {
-      this.pages = response.query.search;
+    this.wikipedia.search(term).subscribe((pages) => {
+      this.pages = pages;
     });
   }
 }
